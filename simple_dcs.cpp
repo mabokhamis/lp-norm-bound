a25f0220546edb7653c46867535b1c38222b0e52
mabokhamis <mahmoudabo@gmail.com>
2024-08-01 17:35:49 -0700

    updated translation to HiGHS

--- simple_dcs.cpp
+++ simple_dcs.cpp
@@ -127,9 +127,105 @@ ostream& operator<<(ostream& os, const LP& lp) {
     return os;
 }
 
+// Convert an LP to a HiGHs LpProblem
+pair<double,vector<double>> solve(const LP &p) {
+    int n = p.variables.size();
+    int m = p.constraints.size();
+
+    HighsModel model;
+    model.lp_.num_col_ = n;
+    model.lp_.num_row_ = m;
+    model.lp_.sense_ = p.objective.maximize ? ObjSense::kMaximize : ObjSense::kMinimize;
+    model.lp_.offset_ = 0.0;
+    model.lp_.col_cost_.resize(n);
+    for (const auto& v : p.objective.sum)
+        model.lp_.col_cost_[v.first] = v.second;
+    model.lp_.col_lower_.resize(n);
+    model.lp_.col_upper_.resize(n);
+    int i = 0;
+    for (const auto& v : p.variables) {
+        model.lp_.col_lower_[i] = v.lower_bound;
+        model.lp_.col_upper_[i] = v.upper_bound;
+        ++i;
+    }
+    model.lp_.row_lower_.resize(m);
+    model.lp_.row_upper_.resize(m);
+    i = 0;
+    for (const auto& c : p.constraints) {
+        model.lp_.row_lower_[i] = c.lower_bound;
+        model.lp_.row_upper_[i] = c.upper_bound;
+        ++i;
+    }
+    model.lp_.a_matrix_.format_ = MatrixFormat::kRowwise;
+    model.lp_.a_matrix_.start_.resize(m + 1);
+    i = 0;
+    for (const auto& c : p.constraints) {
+        model.lp_.a_matrix_.start_[i] = model.lp_.a_matrix_.index_.size();
+        for (const auto& t : c.sum) {
+            model.lp_.a_matrix_.index_.push_back(t.first);
+            model.lp_.a_matrix_.value_.push_back(t.second);
+        }
+        ++i;
+    }
+    model.lp_.a_matrix_.start_[i] = model.lp_.a_matrix_.index_.size();
+
+    // Create a Highs instance
+    Highs highs;
+    HighsStatus return_status;
+
+    // Pass the model to HiGHS
+    return_status = highs.passModel(model);
+    assert(return_status==HighsStatus::kOk);
+
+    // Get a const reference to the LP data in HiGHS
+    // const HighsLp& lp = highs.getLp();
+
+    // Solve the model
+    return_status = highs.run();
+    assert(return_status==HighsStatus::kOk);
+
+    // Get the model status
+    const HighsModelStatus& model_status = highs.getModelStatus();
+    assert(model_status==HighsModelStatus::kOptimal);
+
+    const HighsInfo& info = highs.getInfo();
+    const double obj = info.objective_function_value;
+    // const bool has_values = info.primal_solution_status;
+    const HighsSolution& solution = highs.getSolution();
+
+    return make_pair(obj, solution.col_value);
+}
+
+void test_lp1() {
+    LP lp(true);
+    int x = lp.add_variable("x", 0.0, INFINITY);
+    int y = lp.add_variable("y", 0.0, INFINITY);
+    int z = lp.add_variable("z", 0.0, INFINITY);
+    int t = lp.add_variable("t", 0.0, 2.0);
+    int c1 = lp.add_constraint("c1", -INFINITY, 1.0);
+    lp.add_to_constraint(c1, x, 1.0);
+    lp.add_to_constraint(c1, y, 1.0);
+    int c2 = lp.add_constraint("c2", -INFINITY, 1.0);
+    lp.add_to_constraint(c2, y, 1.0);
+    lp.add_to_constraint(c2, z, 1.0);
+    int c3 = lp.add_constraint("c3", -INFINITY, 1.0);
+    lp.add_to_constraint(c3, x, 1.0);
+    lp.add_to_constraint(c3, z, 1.0);
+    lp.add_to_objective(x, 1.0);
+    lp.add_to_objective(y, 1.0);
+    lp.add_to_objective(z, 1.0);
+    lp.add_to_objective(t, 1.0);
+    auto sol = solve(lp);
+    auto& obj = sol.first;
+    auto& val = sol.second;
+    assert(abs(obj - 3.5) < 1e-7);
+    assert(abs(val[x] - 0.5) < 1e-7);
+    assert(abs(val[y] - 0.5) < 1e-7);
+    assert(abs(val[z] - 0.5) < 1e-7);
+    assert(abs(val[t] - 2.0) < 1e-7);
+}
+
 int main() {
-    unordered_map<int, double> m;
-    m[1] += 1;
-    cout << m[1] << endl;
+    test_lp1();
     return 0;
 }
